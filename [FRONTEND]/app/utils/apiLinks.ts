c217a77fb6bfc91bdeaee81f918ce7305547da6b
Ruslan <66295121+BlueRexPY@users.noreply.github.com>
2022-08-10 05:57:51 +0200

    Deploy on google cloud

--- [FRONTEND]/app/utils/apiLinks.ts
+++ [FRONTEND]/app/utils/apiLinks.ts
@@ -1,6 +1,7 @@
-export const DEFUALT_API = "https://prod-tsmusic.herokuapp.com/"
+export const DEFUALT_API = "https://app-uj4ehqai5a-uc.a.run.app/"
 //http://localhost:5000/
 //https://prod-tsmusic.herokuapp.com/
+//https://app-uj4ehqai5a-uc.a.run.app/
 
 export const CONFIG = {
     headers: { 'Access-Control-Allow-Origin': '*' }
