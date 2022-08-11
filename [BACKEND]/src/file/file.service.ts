595b7886234f7520b6f4680284a04226bf60f170
Ruslan <66295121+BlueRexPY@users.noreply.github.com>
2022-08-12 01:35:58 +0200

    v 0.7.1
    Fix bugs

--- [BACKEND]/src/file/file.service.ts
+++ [BACKEND]/src/file/file.service.ts
@@ -14,15 +14,18 @@ export class FileService{
 
     createFile(type: FileType, file): string {
          try {
-            //return `https://storage.googleapis.com/tsmusic-files/9ed606cc-714e-484a-9bc7-9ffe8cb85eb5.jpg`
             const gc = new Storage({ keyFilename: "google-cloud-key.json" });
             gc.getBuckets().then(x => console.log(x))
             const fileExtension = file.originalname.split('.').pop()
             const fileName = uuid.v4() + '.' + fileExtension
             const filePath = path.resolve(__dirname, '..', 'static', type)
+            
+            if(!fs.existsSync(filePath)) {
+                fs.mkdirSync(filePath, {recursive: true})
+            }
             fs.writeFileSync(path.resolve(filePath, fileName), file.buffer)
             
-            gc.bucket("tsmusic-files").upload(filePath+"/"+fileName, {
+            gc.bucket("tsmusic-files").upload(filePath+"//"+fileName, {
                 destination: fileName,
               });
 
