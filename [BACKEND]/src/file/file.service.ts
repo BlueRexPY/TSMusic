b113347aff1f9b940559e58764e62955494beed1
Ruslan <66295121+BlueRexPY@users.noreply.github.com>
2022-08-11 22:48:15 +0200

    v 7.1
    Save files on google cloud

--- [BACKEND]/src/file/file.service.ts
+++ [BACKEND]/src/file/file.service.ts
@@ -2,33 +2,34 @@ import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import * as fs from 'fs'
 import * as path from 'path'
 import * as uuid from 'uuid'
+const { Storage } = require("@google-cloud/storage");
 
 export enum FileType {
     AUDIO = 'audio',
     IMAGE = "image"
 }
 
-
 @Injectable()
 export class FileService{
 
     createFile(type: FileType, file): string {
-        try {
+         try {
+            //return `https://storage.googleapis.com/tsmusic-files/9ed606cc-714e-484a-9bc7-9ffe8cb85eb5.jpg`
+            const gc = new Storage({ keyFilename: "google-cloud-key.json" });
+            gc.getBuckets().then(x => console.log(x))
             const fileExtension = file.originalname.split('.').pop()
             const fileName = uuid.v4() + '.' + fileExtension
             const filePath = path.resolve(__dirname, '..', 'static', type)
-            if(!fs.existsSync(filePath)) {
-                fs.mkdirSync(filePath, {recursive: true})
-            }
             fs.writeFileSync(path.resolve(filePath, fileName), file.buffer)
-            return type + '/' + fileName
+            
+            gc.bucket("tsmusic-files").upload(filePath+"/"+fileName, {
+                destination: fileName,
+              });
+
+            return `https://storage.cloud.google.com/tsmusic-files/${fileName}`
+
         } catch (e) {
             throw new HttpException(e.message, HttpStatus.INTERNAL_SERVER_ERROR)
         }
     }
-
-    removeFile(fileName: string) {
-
-    }
-
 }
