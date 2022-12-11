a7c0ad6a4dda129dd25a4b31631eaa45d030d511
Ruslan <66295121+BlueRexPY@users.noreply.github.com>
2022-12-11 03:53:12 +0100

    Update apiLinks.ts

--- [FRONTEND]/app/utils/apiLinks.ts
+++ [FRONTEND]/app/utils/apiLinks.ts
@@ -1,7 +1,5 @@
-export const DEFAULT_API = "https://prod-tsmusic.herokuapp.com/"
+export const DEFAULT_API = "https://tsmusic.onrender.com/"
 //http://localhost:5000/
-//https://prod-tsmusic.herokuapp.com/
-//https://app-uj4ehqai5a-uc.a.run.app
 
 export const CONFIG = {
     headers: { 'Access-Control-Allow-Origin': '*' }
